2f0ebd03ed1e4c2824597045a64bc0b6c3e92cd6
Ирина Бобина <bobin.ir495@gmail.com>
2020-08-01 13:28:04 +0300

    l

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-
-from django.db import models
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -9,3 +7,11 @@ class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    group = models.ForgeinKey(Group, blank = True, null = True, related_name="posts") #related_name здесь не нужен?
+
+class Group(models.Model):
+    title = models.TextField()
+    slug = models.SlugField() #такой должен быть тип поля?
+    description = models.TextField()
+    def __str__(self):
+        return self.title 
--- posts/views.py
+++ posts/views.py
@@ -5,9 +5,14 @@ from .models import Post
 
 def index(request):
 # одна строка вместо тысячи слов на SQL
-    latest = Post.objects.order_by("-pub_date")[:11]
+    latest = Post.objects.order_by("-pub_date")[:10]
     # собираем тексты постов в один, разделяя новой строкой
-    #output = []
-    #for item in latest:
-    #output.append(item.text)
+    output = []
+    for item in latest:
+        output.append(item.text)
     return render(request, "index.html", {"posts": latest})
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by("-pub_date")[:12] #posts - это список?
+    return render(request, "group.html", {"group": group, "posts": posts})
