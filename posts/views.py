945a10600f0b158774fd0a24194e6a5ac899c9a7
Ирина Бобина <bobin.ir495@gmail.com>
2020-08-01 18:43:05 +0300

    лото

--- posts/views.py
+++ posts/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Post
+from django.shortcuts import get_object_or_404
+from .models import Post, Group
 
 
 def index(request):
 # одна строка вместо тысячи слов на SQL
-    latest = Post.objects.order_by("-pub_date")[:10]
-    # собираем тексты постов в один, разделяя новой строкой
-    output = []
-    for item in latest:
-        output.append(item.text)
+    latest = Post.objects.order_by("-pub_date")[:10] #список их десяти объектов
     return render(request, "index.html", {"posts": latest})
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by("-pub_date")[:12] #posts - это список?
-    return render(request, "group.html", {"group": group, "posts": posts})
+    posts = Post.objects.filter(group=group).order_by("-pub_date")[:12] 
+    context = {
+        "group": group,
+        "posts": posts
+    }
+    return render(request, "group.html", context)
