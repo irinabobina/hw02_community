e063f4c8b828d0877262cfcda29d683f601015c5
Ирина Бобина <bobin.ir495@gmail.com>
2020-08-01 17:18:53 +0300

    55

--- posts/models.py
+++ posts/models.py
@@ -3,15 +3,15 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True) 
+    description = models.TextField()
+    def __str__(self):
+        return self.title 
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
-    group = models.ForgeinKey(Group, blank = True, null = True, related_name="posts") #related_name здесь не нужен?
-
-class Group(models.Model):
-    title = models.TextField()
-    slug = models.SlugField() #такой должен быть тип поля?
-    description = models.TextField()
-    def __str__(self):
-        return self.title 
+    group = models.ForgeinKey(Group, on_delete=models.SET_NULL, blank = True, null = True, related_name="posts")
