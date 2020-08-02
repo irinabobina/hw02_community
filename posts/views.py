331320a4be8959809ef3408c23a8932a7ace01bb
Ирина Бобина <bobin.ir495@gmail.com>
2020-08-02 23:06:45 +0300

    55445

--- posts/views.py
+++ posts/views.py
@@ -4,10 +4,10 @@ from .models import Post, Group
 
 def index(request):
 # одна строка вместо тысячи слов на SQL
-    latest = Post.objects.order_by("-pub_date")[:10] #список их десяти объектов
+    latest = Post.objects.order_by("-pub_date")[:11] #список из объектов
     return render(request, "index.html", {"posts": latest})
 
-def group_posts(request, slug=None):
+def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by("-pub_date")[:12] 
     context = {
