0970bc425ab349f353e49e462a0f48d7f9636a91
Ирина Бобина <bobin.ir495@gmail.com>
2020-08-02 22:59:45 +0300

    ll

--- posts/models.py
+++ posts/models.py
@@ -6,7 +6,7 @@ User = get_user_model()
 class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True) 
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     def __str__(self):
         return self.title 
 
