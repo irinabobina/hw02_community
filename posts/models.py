bae5258ed2777e6408cf73e68a4792dc3fb06705
Ирина Бобина <bobin.ir495@gmail.com>
2020-08-04 16:56:50 +0300

    kjjhkj

--- posts/models.py
+++ posts/models.py
@@ -21,3 +21,7 @@ class Post(models.Model):
                                related_name="posts")
     group = models.ForeignKey(Group, on_delete=models.SET_NULL,
                               blank=True, null=True, related_name="posts")
+
+
+class Meta:
+    ordering = ("-pub_date",)
